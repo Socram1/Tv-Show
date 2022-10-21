88f1d7f27ee679d16e1ce90fc5f1f899745eeb95
Socram1 <106172107+Socram1@users.noreply.github.com>
2022-10-21 11:31:39 -0400

    Add files via upload

---
+++ Data/ShowAppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShowApp.Models;
+
+namespace ShowApp.Data
+{
+    public class ShowAppContext : DbContext
+    {
+        public ShowAppContext (DbContextOptions<ShowAppContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ShowApp.Models.Shows> Shows { get; set; }
+    }
+}
---
+++ dbo.Shows.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Shows] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX) NOT NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    [Year]        INT            NOT NULL,
+    CONSTRAINT [PK_Shows] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
